8c2048d786d598c59e8418eb08fa72ff954a8818
vincent Lemaire <vincent.lemaire@ig2i.centralelille.fr>
2020-03-30 00:42:11 +0200

    correct fixtures load, [Todo] correct article_id -> comment link

--- skeleton/src/DataFixtures/AppFixtures.php
+++ skeleton/src/DataFixtures/AppFixtures.php
@@ -8,37 +8,30 @@ use App\Entity\Article;
 use App\Entity\Comment;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+  public const ARTICLE_ID = 'article_id';
 
-      // use the factory to create a Faker\Generator instance
-      $faker = Faker\Factory::create();
+  public function load(ObjectManager $manager)
+  {
+    // use the factory to create a Faker\Generator instance
+    $faker = Faker\Factory::create();
+    
+    for($i = 0; $i <10; $i++) {
+
+      $article = new Article();
+      $article->setTitle($faker->sentence($nbWords = 3, $variableNbWords = false));
+      $article->setSubtitle($faker->sentence($nbWords = 5, $variableNbWords = false));
+      $article->setCreatedAt($faker->dateTime($max = 'now', $timezone = null));
+      $article->setAuthor($faker->name($gender = 'female'));
+      $article->setBody($faker->paragraph($nbSentences = 5, $variableNbSentences = true));
+      $article->setImage("/img");
       
-      for($i = 0; $i <10; $i++) {
-
-        $article = new Article();
-        $article->setTitle($faker->sentence($nbWords = 3, $variableNbWords = false));
-        $article->setSubtitle($faker->sentence($nbWords = 5, $variableNbWords = false));
-        $article->setCreatedAt($faker->dateTime($max = 'now', $timezone = null));
-        $article->setAuthor($faker->name($gender = 'female'));
-        $article->setBody($faker->paragraph($nbSentences = 5, $variableNbSentences = true));
-        $article->setImage("/img");
-        $article->addComment($this->getReference(CommentFixtures::ARTICLE_ID));
-        $manager->persist($article);
-      }
-      $manager->flush();
+      $manager->persist($article);
     }
-
-    public function getDependencies()
-    {
-        return array(
-            CommentFixtures::class,
-        );
-    }
+    $manager->flush();
+    $this->addReference(self::ARTICLE_ID, $article);
+  }
 }
--- skeleton/src/DataFixtures/CommentFixtures.php
+++ skeleton/src/DataFixtures/CommentFixtures.php
@@ -4,30 +4,37 @@ namespace App\DataFixtures;
 
 use Faker;
 use App\Entity\Comment;
+use App\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 class CommentFixtures extends Fixture
 {
+  public function load(ObjectManager $manager)
+  {
 
-  public const ARTICLE_ID = 'article_id';
+    $faker = Faker\Factory::create();
 
-    public function load(ObjectManager $manager)
+
+    $article = $this->getReference(AppFixtures::ARTICLE_ID);
+    for($i = 0; $i < 5 ; $i++ )
     {
-
-      $faker = Faker\Factory::create();
-      for($i = 0; $i < 20 ; $i++ )
-      {
-        $comment = new Comment();
-        $comment->setName($faker->name());
-        $comment->setEmail($faker->safeEmail());
-        $comment->setCreatedAt($faker->dateTime($max = 'now', $timezone = null));
-        $comment->setComment($faker->paragraph($nbSentences = 2, $variableNbSentences = true));
-        // $article->addComment($comment);
-        $manager->persist($comment);
-      }
-
-      $manager->flush();
-      $this->addReference(self::ARTICLE_ID, $comment);
+      $comment = new Comment();
+      $comment->setName($faker->name());
+      $comment->setEmail($faker->safeEmail());
+      $comment->setCreatedAt($faker->dateTime($max = 'now', $timezone = null));
+      $comment->setComment($faker->paragraph($nbSentences = 2, $variableNbSentences = true));
+      $article->addComment($comment);
+      $manager->persist($comment);
     }
+    $manager->flush();
+  }
+
+  public function getDependencies()
+  {
+      return array(
+          CommentFixtures::class,
+      );
+  }
 }
